10812f2474c376bb0ae3763611520a860eedb437
Bogdan Samonov <bogdansamonov@gmail.com>
2022-07-09 13:48:14 +0300

    style: add style to Error404 component

--- calculator/src/App/index.jsx
+++ calculator/src/App/index.jsx
@@ -4,16 +4,16 @@ import { Switch, Route } from 'react-router-dom'
 import { Pages } from '@/constants'
 
 import Loader from '@/components/Loader'
-import Main from '@/containers/Main'
-import Settings from '@/srceens/Settings'
 
-const { CALCULATOR, HOME_PAGE_ROUTE, SETTINGS } = Pages
-
-const HomePage = lazy(() => import('@/pages/Home'))
 const Calculator = lazy(() =>
   import('@/srceens/Calculator'),
 )
+const Main = lazy(() => import('@/containers/Main'))
 const Header = lazy(() => import('@/containers/Header'))
+const Settings = lazy(() => import('@/srceens/Settings'))
+const Error404 = lazy(() => import('@/srceens/Error404'))
+
+const { CALCULATOR, ERROR, SETTINGS } = Pages
 
 export default () => (
   <Suspense fallback={<Loader />}>
@@ -27,6 +27,8 @@ export default () => (
         />
         <Route exact path={SETTINGS}
 component={Settings} />
+        <Route exact path={ERROR}
+component={Error404} />
       </Switch>
     </Main>
   </Suspense>
--- calculator/src/constants/message.js
+++ calculator/src/constants/message.js
@@ -2,3 +2,5 @@ export const calcMessage = {
   errorExpression: 'Uncorrect expression',
   errorValue: 'Uncorrect value',
 }
+
+export const error404Message = "Page isn't found :("
